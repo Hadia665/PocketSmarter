ed75d7a5dcc4ee975d020edb16b09d3ef23b2f1e
Hadia665 <hadiaawan665@gmail.com>
2026-05-07 00:16:16 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 load_dotenv('database.env')
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-print("URL:", SUPABASE_URL)  # check karo
-print("KEY:", SUPABASE_KEY)  # check karo
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+SUPABASE_URL=os.getenv("SUPABASE_URL")
+SUPABASE_KEY=os.getenv("SUPABASE_KEY")
+print("URL:",SUPABASE_URL)
+print("KEY:",SUPABASE_KEY)
+supabase=create_client(SUPABASE_URL,SUPABASE_KEY)
 print("Connected!")
