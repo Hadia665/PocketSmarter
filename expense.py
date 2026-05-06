65f69fd3eddc36848b409c4f1692bccd45bb9f36
Hadia665 <hadiaawan665@gmail.com>
2026-05-07 00:18:02 +0500

    Update expense.py

--- expense.py
+++ expense.py
@@ -10,8 +10,7 @@ def submitExpenses(username,extraadded,extraminus):
     if saved<0:
         walletVal=float(getWallet(username) or 0)
         newWallet=walletVal+saved
-        supabase.table('wallet').update({'total_saved': newWallet
-        }).eq('username',username).execute()
+        supabase.table('wallet').update({'total_saved': newWallet}).eq('username',username).execute()
         saved=0
     updateSaved(username,saved)
     return saved
